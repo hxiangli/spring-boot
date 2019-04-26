97eb224850d4b64064a78b00957c46a503cc59db
hxiangli <hxiangli@qq.com>
2019-04-26 15:43:58 +0800

    netty , springsecrity ,

--- src/main/java/com/hlfc/springboot/SpringbootApplication.java
+++ src/main/java/com/hlfc/springboot/SpringbootApplication.java
@@ -4,20 +4,12 @@ package com.hlfc.springboot;
  * spring boot 启动类
  */
 
-import org.beetl.core.resource.WebAppResourceLoader;
-import org.beetl.ext.spring.BeetlGroupUtilConfiguration;
-import org.beetl.ext.spring.BeetlSpringViewResolver;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.core.io.support.ResourcePatternResolver;
-import org.springframework.core.io.support.ResourcePatternUtils;
-
-import java.io.IOException;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.hlfc")
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
