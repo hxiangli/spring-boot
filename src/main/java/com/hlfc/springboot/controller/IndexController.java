4447d33599b9b0f53a4d978e16c8ac1934a418ff
hxiangli <305094386@qq.com>
2023-09-07 10:01:35 +0800

    登录成功页11

--- src/main/java/com/hlfc/springboot/controller/IndexController.java
+++ src/main/java/com/hlfc/springboot/controller/IndexController.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class IndexController {
     /**
-     * 登录成功页
+     * 登录成功页11
      * @return
      */
     @RequestMapping("/index")
